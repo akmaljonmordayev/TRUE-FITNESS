44135697633bddbed2ce817f234dfbbec5fee725
adhamjon0770 <isagi3572@gmail.com>
2025-07-26 12:36:44 +0500

    section1-div2 qoshildi

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IoTriangle } from "react-icons/io5";
 import "./home.css";
+import Line2 from "../../assets/images/Line 2.png";
 function Home() {
   return (
     <>
@@ -8,9 +9,24 @@ function Home() {
         <section className="a-section1">
           <div className="a-section1-div1">
             <button className="video-btn">
-              <a target="blank" href="https://youtu.be/HztGXJqdBrk?si=l-w85MN4bBRTKJfM"><IoTriangle /></a>
+              <a
+                target="blank"
+                href="https://youtu.be/HztGXJqdBrk?si=l-w85MN4bBRTKJfM"
+              >
+                <IoTriangle />
+              </a>
             </button>
-            <h1 className="a-section1-div1-h1">TRUE FITNESS - ПРЕМИУМ ТРЕНАЖЕРЫ ИЗ США </h1>
+            <h1 className="a-section1-div1-h1">
+              TRUE FITNESS - ПРЕМИУМ ТРЕНАЖЕРЫ ИЗ США{" "}
+            </h1>
+          </div>
+          <div className="a-section1-div2">
+            <img src={Line2} alt="" />
+            <h1 className="a-section1-div2-h1">
+              TRUE - ведущий мировой производитель премиального фитнес
+              оборудования
+            </h1>
+            <img src={Line2} alt="" />
           </div>
         </section>
       </main>
