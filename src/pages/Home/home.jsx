d427c85875c7a14ee9cf7d4e1322b7048a2b8709
adhamjon0770 <isagi3572@gmail.com>
2025-07-26 16:58:59 +0500

    asda

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { IoTriangle } from "react-icons/io5";
 import "./home.css";
 import Line2 from "../../assets/images/Line 2.png";
+import Treadmill from "../../assets/images/treadmill.png";
 function Home() {
   return (
     <>
@@ -27,6 +28,35 @@ function Home() {
               оборудования
             </h1>
             <img src={Line2} alt="" />
+          </div>
+          <div className="banner">
+            <div className="banner-text">
+              <h1>
+                <span className="bold">TRUE</span>{" "}
+                <span className="blue">FITNESS</span>
+              </h1>
+              <div className="line"></div>
+              <p>
+                СОВРЕМЕННОЕ И<br />
+                НАДЁЖНОЕ
+                <br />
+                ОБОРУДОВАНИЕ ДЛЯ
+                <br />
+                ФИТНЕС-КЛУБОВ
+              </p>
+              <a href="#" className="cardio-link">
+                <span>КАРДИО</span> ТРЕНАЖЕРЫ →
+              </a>
+            </div>
+             <div className="dots">
+              <span className="dot active"></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+            <div className="banner-img">
+              <img src={Treadmill} alt="Treadmill" />
+            </div>
           </div>
         </section>
       </main>
