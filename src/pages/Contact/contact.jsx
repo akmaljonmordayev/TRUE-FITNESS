8a04323c36a7e36d50ed349ec92ea086e6f0b200
Myruziyev <myruziyev@gmail.com>
2025-07-28 17:34:07 +0500

    contact page

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -19,8 +19,8 @@ function Contact() {
           <div className="map-container">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.374837736036!2d69.24062237678935!3d41.31133331558252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38aef4f1f1b1b1b1%3A0x1b1b1b1b1b1b1b1b!2sFurqat%20st%202A%2C%20Tashkent!5e0!3m2!1sen!2s!4v1718900000000!5m2!1sen!2s"
-              width="100%"
-              height="450"
+              width="662px"
+              height="662px"
               style={{ border: 0, borderRadius: "8px" }}
               allowFullScreen=""
               loading="lazy"
@@ -42,7 +42,7 @@ function Contact() {
                 Адрес: Ташкент Сити, Булевард, ул. Фурката 2А
               </p>
             </div>
-            <h2 className="text-8">ПН-СБ с 9:00 до 19:00 | ВС — выходной</h2>
+            <h2 className="text-8">ПН-СБ с 9:00-19:00 ВС НЕ РАБОЧИЙ</h2>
           </div>
         </div>
       </div>
